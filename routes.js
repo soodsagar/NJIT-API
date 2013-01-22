104b8cf7b3a645bc95147ad930be05e4d95158b2
Arthur Maciejewicz <arthur.maciejewicz@gmail.com>
2013-01-22 04:20:41 -0500

    Added config, and query routes
    For now, input is not escaped or sanitized. Valid routes
    for the web server are described in routes.js
    When logic grows, this will be moved out to controllers.

--- routes.js
+++ routes.js
@@ -1,5 +1,65 @@
-module.exports = function(app) {
-  app.get('/', function(req, res) {
-    res.send({test: "GOOD!"});
+var mongodb = require('mongodb');
+
+module.exports = function(app, db) {
+
+  app.get('/classes', function(req, res) {
+
+    var courses = db.collection('courses');
+    courses.find({}).toArray( function(err, results) {
+      if(err) {
+        console.log(err);
+        return res.send({error: "DB error"});
+      }
+      res.send(results);
+    });
+  });
+
+  // find a course by call ID
+  app.get('/classes/call/:call', function(req, res) {
+
+    var courses = db.collection('courses');
+    var call_number = req.param('call');
+
+    courses.find({call: call_number}).toArray(function(err, results) {
+      if(err) {
+        console.log(err)
+        return res.send({err:"DB error"});
+      }
+      res.send(results);
+    });
+  });
+
+  app.get('/classes/subject/:subject', function(req, res) {
+
+    var courses = db.collection('courses');
+    var subject = req.param('subject');
+    courses.find({subject:subject}).toArray(function(err, results) {
+      if(err) {
+        console.log(err);
+        return res.send({error: "DB error"});
+      }
+      res.send(results);
+    });
+  });
+
+  app.get('/classes/subject/:subject/course/:course_id', function(req, res) {
+    var classes = db.collection('courses');
+    var subject = req.param('subject');
+    var course_id = req.param('course_id');
+
+    db.collection('courses').find({
+      subject: subject,
+      course: course_id
+    }).toArray(function(err, results) {
+      if(err) {
+        console.log(err);
+        return res.send({error: "DB error"});
+      }
+      res.send(results);
+    });
+  });
+
+  app.get('/*', function(req, res) {
+    res.send({error: "Invalid Route"}, 404);
   });
 }
--- server.js
+++ server.js
@@ -2,8 +2,15 @@
 
 var express = require('express')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , format = require('util').format
+  , config = require('./config.json')
+  , mongodb = require('mongodb')
+  , MongoClient = mongodb.MongoClient;
 
+/*
+ * Environment setup
+ */
 
 var IPADDR = process.env.OPENSHIFT_INTERNAL_IP;
 if (typeof IPADDR === 'undefined') {
@@ -11,6 +18,40 @@ if (typeof IPADDR === 'undefined') {
   IPADDR = "127.0.0.1";
 }
 var PORT   = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+
+/*
+ *
+ * MongoDB setup
+ *
+ */
+
+// by default, connect to local db "njit" without auth
+var MongodbConfig = {
+    USERNAME: process.env.OPENSHIFT_MONGODB_DB_USERNAME || config.mongo.USERNAME
+  , PASSWORD: process.env.OPENSHIFT_MONGODB_DB_PASSWORD || config.mongo.PASSWORD
+  , HOST: process.env.OPENSHIFT_MONGODB_DB_HOST || config.mongo.HOST
+  , PORT: parseInt(process.env.OPENSHIFT_MONGODB_DB_PORT) || config.mongo.PORT
+  , APP_NAME: process.env.OPENSHIFT_APP_NAME || config.mongo.DB
+};
+
+if (MongodbConfig.USERNAME !== '') {
+  var MONGO_URI = format(
+     "mongodb://%s:%s@%s:%s/%s"
+    , MongodbConfig.USERNAME
+    , MongodbConfig.PASSWORD
+    , MongodbConfig.HOST
+    , MongodbConfig.PORT
+    , MongodbConfig.APP_NAME
+  );
+} else {
+  var MONGO_URI = format(
+      "mongodb://%s:%s/%s"
+    , MongodbConfig.HOST
+    , MongodbConfig.PORT
+    , MongodbConfig.APP_NAME
+  );
+}
+    
 
 /**
  *  terminator === the termination handler
@@ -40,28 +81,40 @@ function setupTerminationHandlers () {
   });
 };
 
+function server_start(db) {
+  console.log("Starting server");
+  var app = express();
+
+  app.configure(function(){
+    app.set('port', PORT);
+    app.set('ip', IPADDR);
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+  });
+
+  app.configure('development', function(){
+    app.use(express.errorHandler());
+  });
+
+  http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+  });
+
+  // setup routes
+  require('./routes')(app, db);
+}
+
 setupTerminationHandlers();
 
-var app = express();
-
-app.configure(function(){
-  app.set('port', PORT);
-  app.set('ip', IPADDR);
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+console.log("Connecting mongo to: " + MONGO_URI);
+MongoClient.connect(MONGO_URI, function(err, db) {
+  if(err) throw err;
+  console.log("Mongo connected");
+  server_start(db);
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
 
-http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
-
-// setup routes
-require('./routes')(app);
